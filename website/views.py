31e61ec232729faeccbf59bdce2909c382a5b1ec
Devesh Soni <deveshsoni973@gmail.com>
2024-06-15 22:58:26 +0530

    Adding the login and logouts

--- website/views.py
+++ website/views.py
@@ -4,7 +4,21 @@ from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    #check for logging in
+    if request.method=="POST":
+        username=request.POST('username')
+        password=request.POST('password')
+        user=authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "YOU HAVE BEEN LOGGED IN...")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error. TRY AGAIN")
+            return redirect('home')
+
+    else:
+        return render(request, 'home.html', {})
 
 def login_user(request):
     pass
