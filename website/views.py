061b005efda63dd484d4d51755a957080a66e3db
Devesh Soni <deveshsoni973@gmail.com>
2024-06-15 23:02:58 +0530

    Switched the login logic inside the home.html instead

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
@@ -6,8 +6,8 @@ from django.contrib import messages
 def home(request):
     #check for logging in
     if request.method=="POST":
-        username=request.POST('username')
-        password=request.POST('password')
+        username=request.POST['username']
+        password=request.POST['password']
         user=authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
