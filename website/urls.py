cca5d9e9f8517de9100982d805144555b2597479
Devesh Soni <deveshsoni973@gmail.com>
2024-06-20 14:33:46 +0530

    did some optimizations

--- website/urls.py
+++ website/urls.py
@@ -6,3 +6,6 @@ path('logout/',  views.logout_user, name='logout'),
 path('register/',  views.register_user, name='register'),
 
 ]
+'''In a Django project, the urls.py file is essential for routing URLs to the appropriate views. 
+It acts as a map for the application, directing incoming web requests to the 
+correct view based on the URL pattern. '''
