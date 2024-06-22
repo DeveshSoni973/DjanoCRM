7ed253da187cae06fa6cda16c1fbb449e4d67792
Devesh Soni <deveshsoni973@gmail.com>
2024-06-22 14:14:37 +0530

    Added the Update Functionality and Completed the Project

--- website/urls.py
+++ website/urls.py
@@ -7,6 +7,7 @@ path('register/',  views.register_user, name='register'),
 path('record/<int:pk>', views.customer_record, name="record"),
 path('delete_record/<int:pk>', views.delete_record, name="delete_record"),
 path('add_record/', views.add_record, name="add_record"),
+path('update_record/<int:pk>/', views.update_record, name='update_record'),
 ]
 '''In a Django project, the urls.py file is essential for routing URLs to the appropriate views. 
 It acts as a map for the application, directing incoming web requests to the 
